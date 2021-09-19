b7c7eb70fc9fcd6a828284dce0dfba9c7739b355
Le Dinh <dinhdjj@gmail.com>
2021-09-19 19:27:58 +0700

    build: macro `camel` & `snake` for Arr helper

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use Arr;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +27,23 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Model::preventLazyLoading(!app()->isProduction());
+
+        // Helper to convert keys of array to 'camelCase'
+        Arr::macro('camel', function (array $arr): array {
+            $camelArr = [];
+            foreach ($arr as $key => $value) {
+                $camelArr[Str::camel($key)] = $value;
+            }
+            return $camelArr;
+        });
+
+        // Helper to convert keys of array to 'snack_case'
+        Arr::macro('snake', function (array $arr): array {
+            $camelArr = [];
+            foreach ($arr as $key => $value) {
+                $camelArr[Str::snake($key)] = $value;
+            }
+            return $camelArr;
+        });
     }
 }
