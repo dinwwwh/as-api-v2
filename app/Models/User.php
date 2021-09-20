bd06d274d959838fb2488647547037b43deb444c
Le Dinh <dinhdjj@gmail.com>
2021-09-20 13:31:05 +0700

    build: convert macros of `Auth` to `User` helpers

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,38 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Role::class);
     }
+
+    /**
+     * Determine whether this user has a given permission
+     *
+     */
+    public function hasPermission(string $permissionKey): bool
+    {
+        return $this->permissions()->where('key', $permissionKey)->first(['key'])
+            || $this->roles()->whereRelation('permissions', 'key', $permissionKey)->first(['key']);
+    }
+
+    /**
+     * Determine whether this user has all permissions
+     *
+     */
+    public function hasAllPermissions(array $permissionKeys): bool
+    {
+        foreach ($permissionKeys as $permissionKey) {
+            if (!$this->hasPermission($permissionKey)) return false;
+        }
+        return true;
+    }
+
+    /**
+     * Determine whether this user has any permissions
+     *
+     */
+    public function hasAnyPermissions(array $permissionKeys): bool
+    {
+        foreach ($permissionKeys as $permissionKey) {
+            if ($this->hasPermission($permissionKey)) return true;
+        }
+        return false;
+    }
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -31,36 +31,5 @@ class AuthServiceProvider extends ServiceProvider
         ResetPassword::createUrlUsing(function ($user, string $token) {
             return env('APP_BASE_RESET_PASSWORD_URL', env('APP_URL') . '/reset-password') . '?token=' . $token;
         });
-
-        // Determine whether current auth has a given permission
-        Auth::macro('hasPermission', function (string $permissionKey): bool {
-            if (!$this->check()) return false;
-            if (
-                $this->user()->permissions()->where('key', $permissionKey)->first(['key'])
-            ) return true;
-            if (
-                $this->user()->roles()->whereRelation('permissions', 'key', $permissionKey)->first(['key'])
-            ) return true;
-
-            return false;
-        });
-
-        // Determine whether current auth has all permissions
-        Auth::macro('hasAllPermissions', function (string|array $permissionKeys): bool {
-            if (!$this->check()) return false;
-            foreach ((array)$permissionKeys as $permissionKey) {
-                if (!$this->hasPermission($permissionKey)) return false;
-            }
-            return true;
-        });
-
-        // Determine whether current auth has any permissions
-        Auth::macro('hasAnyPermissions', function (string|array $permissionKeys): bool {
-            if (!$this->check()) return false;
-            foreach ((array)$permissionKeys as $permissionKey) {
-                if ($this->hasPermission($permissionKey)) return true;
-            }
-            return false;
-        });
     }
 }
