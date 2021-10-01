61d9dfd1d6321fd64ed45dc21da451a830290cb0
Le Dinh <dinhdjj@gmail.com>
2021-10-01 09:42:36 +0700

    fix: lack case paginator when `withLoad` call

--- app/Traits/WithLoad.php
+++ app/Traits/WithLoad.php
@@ -3,6 +3,7 @@
 namespace App\Traits;
 
 use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,12 +14,12 @@ trait WithLoad
      * Auto load _relationships and _counts were required
      *
      */
-    public static function withLoad(Collection|Model|Authenticatable $resource)
+    public static function withLoad(Collection|Model|Authenticatable|Paginator $resource)
     {
         $resource->load(request('_relationships', []));
         $resource->loadCount(request('_counts', []));
 
-        if ($resource instanceof Collection) {
+        if ($resource instanceof Collection || $resource instanceof Paginator) {
             return static::collection($resource);
         }
 
