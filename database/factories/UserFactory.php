a15cefa2b07df73e8fb8e057cad8ad78d61def2a
Le Dinh <dinhdjj@gmail.com>
2021-10-07 16:25:32 +0700

    fix: duplicated email when factory user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Account;
 use App\Models\User;
 use Arr;
 use Illuminate\Database\Eloquent\Factories\Factory;
@@ -27,13 +28,23 @@ class UserFactory extends Factory
         $gender = Arr::random(['male', 'female', 'other']);
         $sprite = $gender == 'other' ? 'human' : $gender;
 
+        // Need try/catch since default unit tests don't connect to database
+        try {
+            do {
+                $email = $this->faker->unique()->safeEmail();
+            } while (User::where('email', $email)->first());
+        } catch (\Throwable $th) {
+            //throw $th;
+            $email = $this->faker->unique()->safeEmail();
+        }
+
         return [
             'name' => $name,
             'balance' => rand(10000, 500000),
             'login' => Str::random(),
             'gender' => $gender,
             'avatar_path' => "https://avatars.dicebear.com/api/$sprite/$name.svg",
-            'email' => $this->faker->unique()->safeEmail(),
+            'email' => $email,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
